3daea6486f6a428d6406ba64fa28387da81dc15f
Ajay Wagh <54326598+Ajaywagh2298@users.noreply.github.com>
2022-08-05 21:54:16 +0530

    changes program

--- ProblemSolving/problem2.js
+++ ProblemSolving/problem2.js
@@ -12,15 +12,16 @@ function searchInArray(arr, ele) {
     return -1;
 
 }
-function recursiveFunction(arr, ele, start, end) {
+
+function searchWithrecursiveFunction(arr, ele, start, end) {
     const mid = Math.floor((start + end) / 2);
     console.log(`Middle = ${mid}, Start = ${start}, End = ${end}`);
     if (start > end) return false;
     if (arr[mid]===ele) return mid;
     if(arr[mid] > ele)
-        return recursiveFunction(arr, ele, start, mid-1);
+        return searchWithrecursiveFunction(arr, ele, start, mid-1);
     else
-        return recursiveFunction(arr, ele, mid+1, end);
+        return searchWithrecursiveFunction(arr, ele, mid+1, end);
 }
 
 function dataIndex(){
@@ -28,10 +29,10 @@ function dataIndex(){
     const n = 8;
     const len = arr.length-1;
     const search = searchInArray(arr, n);
-    const searchValue = recursiveFunction(arr, n,0,len)
+    const searchValue = searchWithrecursiveFunction(arr, n,0,len)
 
-    console.log(`Array Index: ${search} for ${n}`);
-    console.log(`Array Index: ${searchValue} for ${n}`);
+    console.log(`Array Index: ${search} for ${n} using 'searchValue()'`);;
+    console.log(`Array Index: ${searchValue} for ${n} using 'searchWithrecursiveFunction()'`);
 }
 
 dataIndex();
@@ -40,10 +41,10 @@ dataIndex();
 /*
 Output of the Solutions are
 PS D:\Developer_student\ProblemSolving> node problem2.js
-    [
-    1, 2, 3, 4, 5,
-        6, 7, 8, 9
-    ]
+[
+  1, 2, 3, 4, 5,
+  6, 7, 8, 9
+]
 1--->2
 2--->3
 3--->4
@@ -56,7 +57,9 @@ Searched Conditon is True : 8--->8
 Middle = 4, Start = 0, End = 8
 Middle = 6, Start = 5, End = 8
 Middle = 7, Start = 7, End = 8
-Array Index: 7 for 8
-    Array Index: 7 for 8
+Array Index: 7 for 8 using 'searchValue()'
+Array Index: 7 for 8 using 'searchWithrecursiveFunction()'
+PS D:\Developer_student\ProblemSolving>
+
 */
 
